e352b3b572e4b681a52980913291d2c94ec910e5
Rafal Seifert <seifert.private@gmail.com>
2022-04-08 15:52:28 +0200

    Basic file structure and components added

---
+++ src/app/calculator/calculator.module.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { CalculatorComponent } from 'src/app/calculator/components/calculator/calculator.component';
+import { FormComponent } from 'src/app/calculator/components/form/form.component';
+import { TableComponent } from 'src/app/calculator/components/table/table.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CalculatorComponent,
+  },
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  declarations: [CalculatorComponent, FormComponent, TableComponent],
+})
+export class CalculatorModule {}
---
+++ src/app/calculator/components/table/table.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css'],
+})
+export class TableComponent {}
