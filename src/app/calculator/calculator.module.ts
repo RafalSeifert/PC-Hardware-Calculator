8dd078fc269531149b2faad52458c958cfe117d6
Rafal Seifert <seifert.private@gmail.com>
2022-04-09 21:37:22 +0200

    Create layout for calculator's form

--- src/app/calculator/calculator.module.ts
+++ src/app/calculator/calculator.module.ts
@@ -2,9 +2,19 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { CalculatorComponent } from 'src/app/calculator/components/calculator/calculator.component';
 import { FormComponent } from 'src/app/calculator/components/form/form.component';
 import { TableComponent } from 'src/app/calculator/components/table/table.component';
+
+// Material Modules
+
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
 const routes: Routes = [
   {
@@ -19,6 +29,12 @@ const routes: Routes = [
     RouterModule.forChild(routes),
     FormsModule,
     ReactiveFormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    FlexLayoutModule,
   ],
   declarations: [CalculatorComponent, FormComponent, TableComponent],
 })
